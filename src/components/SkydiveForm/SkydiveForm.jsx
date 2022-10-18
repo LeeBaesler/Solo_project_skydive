fb872a358dc42fc4b7502589c9fdcbbea5846edc
Lee Baesler <lee.baesler@gmail.com>
2022-10-18 01:08:12 -0500

    created useStates for entries that will be used as inputs

---
+++ src/components/SkydiveForm/SkydiveForm.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function SkydiveForm(props) {
+    const [jumpNumber, setJumpNumber] = useState('');
+    const [date, setDate] = useState('');
+    const [place, setPlace] = useState('');
+    const [aircraft, setAircraft] = useState('');
+    const [equipment, setEquipment] = useState('');
+    const [altitude, setAltitude] = useState('');
+    const [freefall, setFreefall] = useState('');
+    const [totalFreeFall, setTotalFreefall] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+    const [video, setVideo] = useState('');
+    const dispatch = useDispatch();
+    const history = useHistory()
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        console.log(`Adding Log Entry`, {altitude, freefall, description});
+
+        dispatch({
+            type: 'ADD_LOG',
+            
+        })
+    }
+}
