83412b989175ac32009ab3b1977a41c39db12c1f
Lee Baesler <lee.baesler@gmail.com>
2022-10-20 00:06:21 -0500

    DELETE request in server operational

--- server/routes/logbook.router.js
+++ server/routes/logbook.router.js
@@ -54,7 +54,15 @@ router.put ('/:logbookid', (req,res) => {
         });
 })
 
-
+router.delete('/:id', (req,res) => {
+    pool.query('DELETE FROM "log_book" WHERE id=$1', [req.params.id])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error in DELETE log', error);
+        res.sendStatus(500);
+    })
+});
 
 
 
